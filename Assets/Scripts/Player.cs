4c9793f3310fa39ef0ae7702c7e20ec406249520
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-06-27 00:38:25 +0530

    Added player horizontal movement
    Added player horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float runSpeed;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+        float inputValue;
+        if ((inputValue=CrossPlatformInputManager.GetAxis("Horizontal")) != 0f)  //Axis value ranges from -1f to 1f and 0 when 
+        {                                                                                                                  //stationary 
+            Run(inputValue);
+        }
+	}
+
+    void Run(float inputValue)
+    {
+        Vector2 playerVelocity = new Vector2(inputValue * runSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x = -1f;
+            transform.localScale = newScale;
+        }
+    }
+}
