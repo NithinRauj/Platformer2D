0961cd6f890613add31d95be4864ae0bd67c73a7
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-06-30 23:23:34 +0530

    Created an enemy
    Created an enemy and added movement and flipping mechanism

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1f;
+    private Rigidbody2D rb;
+    private BoxCollider2D flipCollider;
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        flipCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	
+	void Update () {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+       
+	}
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        FlipSprite();
+    }
+    void FlipSprite()
+    {
+        bool enemyNotReachedEdge=flipCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (!enemyNotReachedEdge)
+        {
+            transform.localScale = new Vector2(-Mathf.Sign(rb.velocity.x), 1f);    //character is flipped as per sign of velocity
+            moveSpeed = -moveSpeed;
+        }
+    }
+}
