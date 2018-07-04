6ea0d5e2048f7cf303a56d42f8429c024db80731
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-07-04 23:42:02 +0530

    Added level portals
    Added level portals for loading the next level

---
+++ Assets/Scripts/LevelPortal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelPortal : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay;
+    [SerializeField] float slowMoFactor=0.2f;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<Player>())
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = slowMoFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+        var curSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curSceneIndex + 1);
+    }
+}
