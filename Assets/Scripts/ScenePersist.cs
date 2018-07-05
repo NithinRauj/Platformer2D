bc061e5f0483662e73bde2e576c4311bd21a4403
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-07-05 23:36:04 +0530

    Solved coin persistence bug
    Solved a bug where coins didn't apper when game was over and restarted again

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+    private int curSceneIndex,startSceneIndex;
+    private void Start()
+    {
+        startSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    private void Awake()
+    {
+        int numScenePersists = FindObjectsOfType<ScenePersist>().Length;
+        if (numScenePersists > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        curSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (startSceneIndex != curSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
